0caa63510662561ad5d5edf127343bb407019b3f
Adam Shannon <adamkshannon@gmail.com>
2018-01-27 15:13:39 -0600

    go doc fmt

--- doc.go
+++ doc.go
@@ -4,15 +4,17 @@
 // or vendor it into your project.
 //
 // Once added to your project there are two ways to contruct a Client
-// `namecheap.New()`: reads environmental variables
-// `namecheap.NewClient(username, apiuser string, token string, ip string, useSandbox)`
+//
+//     namecheap.New() // reads environmental variables
+//     namecheap.NewClient(username, apiuser string, token string, ip string, useSandbox)
 //
 // The following environmental variables are supported:
-// NAMECHEAP_USERNAME        Username: e.g. adamdecaf
-// NAMECHEAP_API_USER        ApiUser: e.g. adamdecaf
-// NAMECHEAP_API_TOKEN       From https://ap.www.namecheap.com/Profile/Tools/ApiAccess
-// NAMECHEAP_WHITELISTED_IP  Your IP (must be whitelisted)
-// NAMECHEAP_USE_SANDBOX     Use sandbox environment
+//
+//     NAMECHEAP_USERNAME        Username: e.g. adamdecaf
+//     NAMECHEAP_API_USER        ApiUser: e.g. adamdecaf
+//     NAMECHEAP_API_TOKEN       From https://ap.www.namecheap.com/Profile/Tools/ApiAccess
+//     NAMECHEAP_WHITELISTED_IP  Your IP (must be whitelisted)
+//     NAMECHEAP_USE_SANDBOX     Use sandbox environment
 //
 // The public methods are viewable here: https://godoc.org/github.com/adamdecaf/namecheap
 //
