9ee560dba655573e1551a96444d53877f53d29cc
Vlad Fedosov <vladlen.f@namecheap.com>
2021-11-29 18:27:32 +0200

    Feat/added domain get info (#33)
    * feat: added namecheap.domains.getInfo API support
    
    * fix: fixed work with FreeDNS domains for namecheap.domains.dns.getList
    
    * chore: added unit tests for new functionality

--- namecheap/domains_dns_get_list_test.go
+++ namecheap/domains_dns_get_list_test.go
@@ -139,4 +139,84 @@ func TestDomainsDNSGetList(t *testing.T) {
 
 		assert.Nil(t, result.DomainDNSGetListResult.Nameservers)
 	})
+
+	t.Run("FreeDNS domain handling", func(t *testing.T) {
+		fakeDnsGetListResponse := `
+			<?xml version="1.0" encoding="utf-8"?>
+			<ApiResponse Status="ERROR" xmlns="http://api.namecheap.com/xml.response">
+				<Errors>
+					<Error Number="2019166">Domain name not found</Error>
+				</Errors>
+				<Warnings />
+				<RequestedCommand>namecheap.domains.dns.getlist</RequestedCommand>
+				<CommandResponse Type="namecheap.domains.dns.getList" />
+				<Server>PHX01APIEXT11</Server>
+				<GMTTimeDifference>--5:00</GMTTimeDifference>
+				<ExecutionTime>0.074</ExecutionTime>
+			</ApiResponse>
+		`
+
+		fakeGetInfoResponse := `
+			<?xml version="1.0" encoding="utf-8"?>
+			<ApiResponse Status="OK" xmlns="http://api.namecheap.com/xml.response">
+				<Errors />
+				<Warnings />
+				<RequestedCommand>namecheap.domains.getinfo</RequestedCommand>
+				<CommandResponse Type="namecheap.domains.getInfo">
+					<DomainGetInfoResult ID="1706717" DomainName="horse-family.com.ua" OwnerName="NCStaffvladlenf" IsOwner="false" IsPremium="false">
+						<DomainDetails>
+							<CreatedDate>11/26/2021</CreatedDate>
+							<NumYears>0</NumYears>
+						</DomainDetails>
+						<LockDetails />
+						<Whoisguard Enabled="NotAlloted">
+							<ID>0</ID>
+						</Whoisguard>
+						<PremiumDnsSubscription>
+							<UseAutoRenew>false</UseAutoRenew>
+							<SubscriptionId>-1</SubscriptionId>
+							<CreatedDate>0001-01-01T00:00:00</CreatedDate>
+							<ExpirationDate>0001-01-01T00:00:00</ExpirationDate>
+							<IsActive>false</IsActive>
+						</PremiumDnsSubscription>
+						<DnsDetails ProviderType="FreeDNS" IsUsingOurDNS="true" HostCount="0" EmailType="No Email Service" DynamicDNSStatus="false" IsFailover="false">
+							<Nameserver>freedns1.registrar-servers.com</Nameserver>
+							<Nameserver>freedns2.registrar-servers.com</Nameserver>
+						</DnsDetails>
+						<Modificationrights />
+					</DomainGetInfoResult>
+				</CommandResponse>
+				<Server>PHX01APIEXT12</Server>
+				<GMTTimeDifference>--5:00</GMTTimeDifference>
+				<ExecutionTime>0.013</ExecutionTime>
+			</ApiResponse>
+		`
+
+		mockServer := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+			body, _ := ioutil.ReadAll(request.Body)
+			query, _ := url.ParseQuery(string(body))
+			if query.Get("Command") == "namecheap.domains.dns.getList" {
+				_, _ = writer.Write([]byte(fakeDnsGetListResponse))
+			} else {
+				_, _ = writer.Write([]byte(fakeGetInfoResponse))
+			}
+		}))
+		defer mockServer.Close()
+
+		client := setupClient(nil)
+		client.BaseURL = mockServer.URL
+
+		result, err := client.DomainsDNS.GetList("horse-family.com.ua")
+		if err != nil {
+			t.Fatal("Unable to get domains", err)
+		}
+
+		assert.Equal(t, true, *result.DomainDNSGetListResult.IsUsingFreeDNS)
+		assert.Equal(t, false, *result.DomainDNSGetListResult.IsPremiumDNS)
+		assert.Equal(t, true, *result.DomainDNSGetListResult.IsUsingOurDNS)
+		assert.Equal(t, "horse-family.com.ua", *result.DomainDNSGetListResult.Domain)
+
+		expectedNameservers := &[]string{"freedns1.registrar-servers.com", "freedns2.registrar-servers.com"}
+		assert.Equal(t, expectedNameservers, result.DomainDNSGetListResult.Nameservers)
+	})
 }
---
+++ namecheap/domains_get_info_test.go
@@ -0,0 +1,149 @@
+package namecheap
+
+import (
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func TestDomainsGetInfo(t *testing.T) {
+	fakeResponse := `
+		<?xml version="1.0" encoding="utf-8"?>
+		<ApiResponse Status="OK" xmlns="http://api.namecheap.com/xml.response">
+			<Errors />
+			<Warnings />
+			<RequestedCommand>namecheap.domains.getinfo</RequestedCommand>
+			<CommandResponse Type="namecheap.domains.getInfo">
+				<DomainGetInfoResult ID="1706717" DomainName="horse-family.com.ua" OwnerName="NCStaffvladlenf" IsOwner="false" IsPremium="false">
+					<DomainDetails>
+						<CreatedDate>11/26/2021</CreatedDate>
+						<NumYears>0</NumYears>
+					</DomainDetails>
+					<LockDetails />
+					<Whoisguard Enabled="NotAlloted">
+						<ID>0</ID>
+					</Whoisguard>
+					<PremiumDnsSubscription>
+						<UseAutoRenew>false</UseAutoRenew>
+						<SubscriptionId>-1</SubscriptionId>
+						<CreatedDate>0001-01-01T00:00:00</CreatedDate>
+						<ExpirationDate>0001-01-01T00:00:00</ExpirationDate>
+						<IsActive>false</IsActive>
+					</PremiumDnsSubscription>
+					<DnsDetails ProviderType="FreeDNS" IsUsingOurDNS="true" HostCount="0" EmailType="No Email Service" DynamicDNSStatus="false" IsFailover="false">
+						<Nameserver>freedns1.registrar-servers.com</Nameserver>
+						<Nameserver>freedns2.registrar-servers.com</Nameserver>
+						<Nameserver>freedns3.registrar-servers.com</Nameserver>
+						<Nameserver>freedns4.registrar-servers.com</Nameserver>
+						<Nameserver>freedns5.registrar-servers.com</Nameserver>
+					</DnsDetails>
+					<Modificationrights />
+				</DomainGetInfoResult>
+			</CommandResponse>
+			<Server>PHX01APIEXT12</Server>
+			<GMTTimeDifference>--5:00</GMTTimeDifference>
+			<ExecutionTime>0.013</ExecutionTime>
+		</ApiResponse>
+	`
+
+	t.Run("request_command", func(t *testing.T) {
+		var sentBody url.Values
+
+		mockServer := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+			body, _ := ioutil.ReadAll(request.Body)
+			query, _ := url.ParseQuery(string(body))
+			sentBody = query
+			_, _ = writer.Write([]byte(fakeResponse))
+		}))
+		defer mockServer.Close()
+
+		client := setupClient(nil)
+		client.BaseURL = mockServer.URL
+
+		_, err := client.Domains.GetInfo("horse-family.com.ua")
+		if err != nil {
+			t.Fatal("Unable to get domains", err)
+		}
+
+		assert.Equal(t, "namecheap.domains.getInfo", sentBody.Get("Command"))
+	})
+
+	t.Run("server_empty_response", func(t *testing.T) {
+		fakeLocalResponse := ""
+
+		mockServer := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+			_, _ = writer.Write([]byte(fakeLocalResponse))
+		}))
+		defer mockServer.Close()
+
+		client := setupClient(nil)
+		client.BaseURL = mockServer.URL
+
+		_, err := client.DomainsDNS.GetHosts("horse-family.com.ua")
+
+		assert.EqualError(t, err, "unable to parse server response: EOF")
+	})
+
+	t.Run("server_non_xml_response", func(t *testing.T) {
+		fakeLocalResponse := "non-xml response"
+
+		mockServer := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+			_, _ = writer.Write([]byte(fakeLocalResponse))
+		}))
+		defer mockServer.Close()
+
+		client := setupClient(nil)
+		client.BaseURL = mockServer.URL
+
+		_, err := client.DomainsDNS.GetHosts("domain.net")
+
+		assert.EqualError(t, err, "unable to parse server response: EOF")
+	})
+
+	t.Run("server_broken_xml_response", func(t *testing.T) {
+		fakeLocalResponse := "<broken></xml><response>"
+
+		mockServer := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+			_, _ = writer.Write([]byte(fakeLocalResponse))
+		}))
+		defer mockServer.Close()
+
+		client := setupClient(nil)
+		client.BaseURL = mockServer.URL
+
+		_, err := client.DomainsDNS.GetHosts("domain.net")
+
+		assert.EqualError(t, err, "unable to parse server response: expected element type <ApiResponse> but have <broken>")
+	})
+
+	t.Run("server_respond_with_error", func(t *testing.T) {
+		fakeLocalResponse := `
+			<?xml version="1.0" encoding="utf-8"?>
+			<ApiResponse Status="ERROR" xmlns="http://api.namecheap.com/xml.response">
+				<Errors>
+					<Error Number="2050900">Invalid Address</Error>
+				</Errors>
+				<Warnings />
+				<RequestedCommand>namecheap.domains.dns.getlist</RequestedCommand>
+				<Server>PHX01SBAPIEXT05</Server>
+				<GMTTimeDifference>--4:00</GMTTimeDifference>
+				<ExecutionTime>0.011</ExecutionTime>
+			</ApiResponse>
+		`
+
+		mockServer := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+			_, _ = writer.Write([]byte(fakeLocalResponse))
+		}))
+		defer mockServer.Close()
+
+		client := setupClient(nil)
+		client.BaseURL = mockServer.URL
+
+		_, err := client.DomainsDNS.GetHosts("domain.net")
+
+		assert.EqualError(t, err, "Invalid Address (2050900)")
+	})
+}
